9bc68460c4db742fe81b90cbbb08478a0468fc7f
Manohin <111537419+Manohin@users.noreply.github.com>
2023-04-19 22:19:57 +0300

    Initial Commit

---
+++ TrafficLight2/TrafficLight2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight2App.swift
+//  TrafficLight2
+//
+//  Created by Alexey Manokhin on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
