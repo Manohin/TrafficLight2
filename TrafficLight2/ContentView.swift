4d6266a42d84b59cbd3219fe789bb6cbecdfe0a4
Manohin <111537419+Manohin@users.noreply.github.com>
2023-04-20 00:06:17 +0300

    Prepare UI

--- TrafficLight2/ContentView.swift
+++ TrafficLight2/ContentView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var redLightStatus = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    Circle()
+                        .foregroundColor(.red)
+                        .overlay(Circle().stroke(lineWidth: 2).colorInvert())
+                        .shadow(color: .red, radius: 20)
+                        .padding(.bottom, 10)
+                    Circle()
+                        .foregroundColor(.yellow)
+                        .overlay(Circle().stroke(lineWidth: 2).colorInvert())
+                        .shadow(color: .yellow, radius: 20)
+                        .padding(.bottom, 10)
+                    Circle()
+                        .foregroundColor(.green)
+                        .overlay(Circle().stroke(lineWidth: 2).colorInvert())
+                        .shadow(color: .green, radius: 20)
+                        .padding(.bottom, 10)
+                }
+                .frame(width: 300, height: 300)
+                Spacer()
+                
+                Button(action: switchLight) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(lineWidth: 3)
+                                .foregroundColor(.white))
+                        Text("NEXT")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    .frame(width: 150, height: 40)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
+
+private func switchLight() {
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
